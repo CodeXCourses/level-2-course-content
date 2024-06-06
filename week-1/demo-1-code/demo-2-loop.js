7ca806f3890864032e334826c385a093238eb9b8
Lewis Benson <tm.lewisbenson@gmail.com>
2024-06-06 11:44:37 -0400

    remove original code

--- week-1/demo-1-code/demo-2-loop.js
+++
@@ -1,9 +0,0 @@
-
-
-console.log("line 5 will find the elements")
-
-let listItems = document.querySelectorAll("#loopy-list>li");
-for(let item of listItems) {
-  console.log("line 8 will change the color");
-  item.style.color = "red";
-}
--- week-9/README.md
+++
@@ -1,3 +0,0 @@
-# Capstone
-
-Conduct code review on real projects for 15 to 30 minutes to get the students used to the feel of code review. After that breakout rooms to work on Capstones.
